c58af681dd2ba8ebf4f872eed8022ef916bdb2cb
Dournbrood <tanner1220002@gmail.com>
2020-01-21 16:09:52 -0500

    MVP completion

--- server.js
+++ server.js
@@ -6,4 +6,61 @@ const server = express();
 
 server.use(express.json());
 
+//GET
+server.get("/api/accounts", (request, response) => {
+    db.select("*").from("accounts")
+        .then((accounts) => {
+            response.status(200).json({ ...accounts });
+        })
+        .catch((error) => {
+            response.status(500).json({ message: "Internal server error. Scream at the devs until this is FIXED." });
+            console.log("\n\n\n !!! *** ~INTERNAL SERVER ERROR~ *** !!!\n", error, "\n\n");
+        })
+});
+
+//POST
+server.post("/api/accounts", (request, response) => {
+    db("accounts").insert({ ...request.body })
+        .then((things) => {
+            response.status(200).json({
+                message: "All good!",
+                stuff: things,
+            });
+        })
+        .catch((error) => {
+            response.status(500).json({ message: "Internal server error. Scream at the devs until this is FIXED." });
+            console.log("\n\n\n !!! *** ~INTERNAL SERVER ERROR~ *** !!!\n", error, "\n\n");
+        })
+})
+
+//PUT
+server.put("/api/accounts", (request, response) => {
+    db("accounts").where("id", "=", request.body.id).update({ ...request.body })
+        .then((things) => {
+            response.status(200).json({
+                message: "All good!",
+                stuff: things,
+            });
+        })
+        .catch((error) => {
+            response.status(500).json({ message: "Internal server error. Scream at the devs until this is FIXED." });
+            console.log("\n\n\n !!! *** ~INTERNAL SERVER ERROR~ *** !!!\n", error, "\n\n");
+        })
+})
+
+//DELET THIS
+server.delete("/api/accounts", (request, response) => {
+    db("accounts").where("id", "=", request.body.id).delete().
+        then((things) => {
+            response.status(200).json({
+                message: "All good!",
+                stuff: things,
+            });
+        })
+        .catch((error) => {
+            response.status(500).json({ message: "Internal server error. Scream at the devs until this is FIXED." });
+            console.log("\n\n\n !!! *** ~INTERNAL SERVER ERROR~ *** !!!\n", error, "\n\n");
+        })
+})
+
 module.exports = server;
